4b8d5b70399f13b0d7138d27b14cd3d7494afc66
Yemzhai <emzhaev00@gmail.com>
2024-11-18 22:46:06 +0500

    add default status as not_active when create subscription

--- src/main/java/kz/bloooom/administration/converter/subscription/SubscriptionCreateDtoConverter.java
+++ src/main/java/kz/bloooom/administration/converter/subscription/SubscriptionCreateDtoConverter.java
@@ -2,6 +2,8 @@ package kz.bloooom.administration.converter.subscription;
 
 import kz.bloooom.administration.domain.dto.subscription.SubscriptionCreateDto;
 import kz.bloooom.administration.domain.entity.Subscription;
+import kz.bloooom.administration.enumeration.subscription_status.SubscriptionStatusCode;
+import kz.bloooom.administration.service.SubscriptionStatusService;
 import kz.bloooom.administration.service.SubscriptionTypeService;
 import kz.bloooom.administration.service.UserService;
 import lombok.AccessLevel;
@@ -18,11 +20,13 @@ public class SubscriptionCreateDtoConverter {
 
     UserService userService;
     SubscriptionTypeService subscriptionTypeService;
+    SubscriptionStatusService subscriptionStatusService;
 
     public Subscription convert(SubscriptionCreateDto source) {
         Subscription target = new Subscription();
         target.setUser(userService.getById(source.getUserId()));
         target.setSubscriptionType(subscriptionTypeService.getById(source.getSubscriptionTypeId()));
+        target.setSubscriptionStatus(subscriptionStatusService.getByCode(SubscriptionStatusCode.NOT_ACTIVE));
         target.setStartTime(source.getStartTime());
         target.setEndTime(source.getEndTime());
         target.setCreatedDate(new Timestamp(System.currentTimeMillis()));
